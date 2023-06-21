c98ce53902adb23c4a0fb6428d3fc0593aa393a8
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-21 10:19:23 +0530

    basics

--- testCode1/testCode1/Program.cs
+++ testCode1/testCode1/Program.cs
@@ -4,7 +4,72 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* int myNum = 5;               
+               double myDoubleNum = 5.99D;  
+               char myLetter = 'D';         
+               bool myBool = true;        
+               string myText = "Hello"; 
+
+
+               long m = 15000000000L;
+               Console.WriteLine(m);
+
+
+               double Num=19.99D;
+               Console.WriteLine(Num);  */
+
+            //type castin
+
+            /*  int myInt = 9;
+                double myDouble = myInt;       
+
+                Console.WriteLine(myInt);     
+                Console.WriteLine(myDouble); 
+
+
+                int myInt = 10;
+    double myDouble = 5.25;
+    bool myBool = true;
+
+    Console.WriteLine(Convert.ToString(myInt));    
+    Console.WriteLine(Convert.ToDouble(myInt));    
+    Console.WriteLine(Convert.ToInt32(myDouble));
+    Console.WriteLine(Convert.ToString(myBool));
+            */
+
+            //Maths
+
+            /*Math.Max(5, 10);
+              Math.Sqrt(64);
+             //tring txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+              Console.WriteLine("The length of the txt string is: " + txt.Length);
+
+              string txt = "Hello World";
+              Console.WriteLine(txt.ToUpper());   
+              Console.WriteLine(txt.ToLower());
+
+              string firstName = "John ";
+              string lastName = "Doe";
+              string name = string.Concat(firstName, lastName);
+              Console.WriteLine(name); */
+
+
+            //interpolation
+            /*string firstName = "John";
+               string lastName = "Doe";
+               string name = $"My full name is: {firstName} {lastName}";
+               Console.WriteLine(name);
+               string myString = "Hello";
+               Console.WriteLine(myString.IndexOf("e"));
+
+               string name = "John Doe";
+               int charPos = name.IndexOf("D");*/
+
+            //spl char
+
+          //string txt = "We are the so-called \"Vikings\" from the north.";
+          //string txt = "It\'s alright.";
+         // string txt = "The character \\ is called backslash.";
         }
     }
 }
