3c5d52de4c5005e40f0cfb4c966fe10ce9986b1e
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-21 10:27:34 +0530

    commit-3

---
+++ testCode1/testCode1/Inheritence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testCode1
+{
+    class Vehicle
+    {
+        public string brand = "Ford";
+        public void honk()
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public string modelName = "Mustang";
+    }
+}
---
+++ testCode1/testCode1/calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testCode1
+{
+    internal class calculator
+    {
+        double result = 0;
+
+            switch (operatorName)
+            {
+                case "+":
+                    result = firstNo + secondNo;
+                    break;
+                case "-":
+                    result = firstNo - secondNo;
+                    break;
+                case "*":
+                    result = firstNo* secondNo;
+                    break;
+                case "/":
+                    result = firstNo / secondNo;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator entered.");
+                    break;
+            }
+
+            return result;
+    }
+}
