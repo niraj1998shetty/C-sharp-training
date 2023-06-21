37965028a17e6296215bca60170af51ecdc2a6d0
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-21 10:56:59 +0530

    encapsulation

---
+++ testCode1/testCode1/Encapsulation.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+
+public class DemoEncap
+{
+
+    
+    private String studentName;
+    private int studentAge;
+
+    
+    public String Name
+    {
+
+        get { return studentName; }
+
+        set { studentName = value; }
+    }
+
+    
+    public int Age
+    {
+
+        get { return studentAge; }
+
+        set { studentAge = value; }
+    }
+}
+
+
+class GFG
+{
+
+    
+    static public void Main()
+    {
+
+        
+        DemoEncap obj = new DemoEncap();
+
+        
+        obj.Name = "
+
+        Ankita & quot;
+        ;
+
+        
+        obj.Age = 21;
+
+        
+        Console.WriteLine(" Name : " + obj.Name);
+        Console.WriteLine(" Age : " + obj.Age);
+    }
+}
