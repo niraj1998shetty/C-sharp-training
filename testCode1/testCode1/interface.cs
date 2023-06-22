ead1c0d8c207c79821ede19da935420a2d0021ab
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-22 09:10:57 +0530

    Add files via upload

---
+++ testCode1/testCode1/interface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testCode1
+{
+    interface Company
+    {
+        void companyMotto();
+    }
+    public class Google : Company
+    {
+        public void companyMotto()
+        {
+            Console.WriteLine("Make life easier with a little help from our products.");
+        }
+    }
+}
