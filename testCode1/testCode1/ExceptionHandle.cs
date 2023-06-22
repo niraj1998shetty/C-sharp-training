b83eddd46c621d00ef6d983ed5c3a277276d6b28
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-22 09:00:25 +0530

    exceptions

---
+++ testCode1/testCode1/ExceptionHandle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testCode1
+{
+    internal class ExceptionHandle
+    {
+        //Exception handling try catch 
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+        Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+    Console.WriteLine("The 'try catch' is finished.");
+}
+    }
+}
