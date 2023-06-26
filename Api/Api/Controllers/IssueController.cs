eda17865f83869083ec4a53bc686d79c49938095
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-26 12:10:45 +0530

    controller

---
+++ Api/Api/Controllers/IssueController.cs
@@ -0,0 +1,25 @@
+﻿using Api.Data;
+using Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<Isssue>> Get()
+        {
+            return await _context.Issues.ToListAsync(); 
+        }
+
+
+    }
+}
