360e2d864e3a12aa68aba96c644e36fd8389deae
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-26 12:37:45 +0530

    Get and post api

--- Api/Api/Controllers/IssueController.cs
+++ Api/Api/Controllers/IssueController.cs
@@ -20,6 +20,25 @@ namespace Api.Controllers
             return await _context.Issues.ToListAsync(); 
         }
 
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Isssue),StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+var issue=await _context.Issues.FindAsync(id);
+            return issue==null ? NotFound() : Ok(issue);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Isssue issue)
+        {
+            await _context.Issues.AddAsync(issue);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue);
+        }
+
 
     }
 }
