1d53a3e529c5f831eb57a077bcf890f29225d310
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-26 15:52:50 +0530

    consuming web api from .net client

---
+++ Api/IssueApp/IssueDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueApp
+{
+    internal class IssueDto
+    {
+        public int Id { get; set; }
+       
+        public string Title { get; set; }
+       
+        public string Description { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public IsuueType isuueType { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? Completed { get; set; }
+    }
+    public enum Priority
+    {
+        Low, Medium, high
+    }
+
+    public enum IsuueType
+    {
+        feature, bug, Documentation
+    }
+
+}
---
+++ Api/IssueApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using IssueApp;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+//using tracking.client;
+
+HttpClient client = new();
+client.BaseAddress = new Uri("http://localhost:5216");
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+HttpResponseMessage response = await client.GetAsync("api/issue");
+response.EnsureSuccessStatusCode();
+
+if (response.IsSuccessStatusCode)
+{
+    var issues = await response.Content.ReadFromJsonAsync<IEnumerable<IssueDto>>();
+
+    foreach (var issue in issues)
+    {
+        Console.WriteLine(issue.Title);
+    }
+}
+else
+{
+    Console.WriteLine("No results");
+}
+Console.ReadLine();
