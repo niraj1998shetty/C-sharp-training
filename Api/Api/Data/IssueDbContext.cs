019b19806f63b17969b6322a7d89e116be4f2351
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-26 10:27:05 +0530

    starting with Apis

---
+++ Api/Api/Data/IssueDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options):base(options)
+        { 
+        
+        }
+
+        public DbSet<Isssue> Issues { get; set; }
+    }
+}
