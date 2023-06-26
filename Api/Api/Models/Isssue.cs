0e178263f630807150f48e27b94a895de0e24bf4
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2023-06-26 12:09:23 +0530

    migration

--- Api/Api/Models/Isssue.cs
+++ Api/Api/Models/Isssue.cs
@@ -1,6 +1,30 @@
-﻿namespace Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models
 {
     public class Isssue
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public IsuueType isuueType { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? Completed { get; set; }
+    }
+    public enum Priority
+    {
+        Low, Medium, high
+    }
+
+    public enum IsuueType
+    {
+        feature , bug, Documentation
     }
 }
